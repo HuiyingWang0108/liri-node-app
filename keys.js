a7d5f89856398f3554dc9cdeed19430afde42e9b
Huiying0108 <whymissu1228@gmail.com>
2018-12-05 14:09:43 -0800

    configure dotenv

---
+++ keys.js
@@ -0,0 +1,8 @@
+// console.log('this is loaded');
+
+// exports.spotify = {
+//   id: process.env.SPOTIFY_ID,
+//   secret: process.env.SPOTIFY_SECRET
+// }
+console.log(process.env.SECRET_MESSAGE); 
+console.log(process.env.TOKEN);
